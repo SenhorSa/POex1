2e38eae7b6ccb32bc569678ba1fd01f27349e769
SenhorSa <al81043@alunos.utad.pt>
2024-04-06 16:38:27 +0100

    1st alteration

--- Source.cpp
+++ Source.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 //instanciar(criar) um objeto da classe data_ok
 int main(){   
-	Data_ok Data_lindinha;
-	Data_lindinha.setDia(6);
-	Data_lindinha.setMes(1);
-	Data_lindinha.setAno(2005);
-	/*Data_lindinha.getDia();
-	Data_lindinha.getMes();
-	Data_lindinha.getAno();*/
-	cout << Data_lindinha.getDia() << "/" << Data_lindinha.getMes() << "/" << Data_lindinha.getAno() << endl;
+	Data_ok Nuno;
+	Nuno.setDia(6);
+	Nuno.setMes(1);
+	Nuno.setAno(2005);
+	Data_ok Jota(7,8,1998);
+	cout << Nuno.getDia() << "/" << Nuno.getMes() << "/" << Nuno.getAno() << endl;
 	//primeiro metodo é o construtor
-
+	cout << Jota.getDia() << "/" << Jota.getMes() << "/" << Jota.getAno() << endl;
 }
